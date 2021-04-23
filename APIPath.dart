a2d896ae4521c008c1c4060fcce71eb975ddcc23
brijesh1012 <62094997+brijesh1012@users.noreply.github.com>
2021-04-23 10:09:25 +0530

    Delete APIPath.dart

--- APIPath.dart
+++
@@ -1,7 +0,0 @@
-class APIPath{
-  //static String user(String uid) => 'users/$uid';
-  static String addToFavorite(String uid ) => 'users/$uid';
-  static String pro(String productId) =>'product/$productId';
-  static String pros() => 'product';
-
-}
